adcbaf5125afea1c9d804c7794565788d19c34f2
ciceroroniel <cicero.roniel@gmail.com>
2020-01-29 20:45:11 -0300

    Prettir ADD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Clique no link para ir ao meu Perfil Profissional:
-        </p>
-        <a
-          className="App-link"
-          href="https://linkedin.com/in/ciceroroniel/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go To Linkedin
-        </a>
-      </header>
-    </div>
+    <h1>Testando!!</h1>
   );
 }
 
